ad9cbeebc1d752daa2288a84040b30c4937ec1de
cguisao <cguisao@fau.edu>
2018-03-28 02:35:37 -0400

    Login frame without any logic.

--- src/com/cop/client/Client/src/client/Client.java
+++ src/com/cop/client/Client/src/client/Client.java
@@ -16,6 +16,10 @@ public class Client {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        loginPanel login = new loginPanel();
+        login.createGui();;
+        
     }
     
 }
